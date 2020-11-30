f8afc204e8ba8445510b8a13014e5dec81e8fb74
noliahmed <74485398+noliahmed@users.noreply.github.com>
2020-11-30 20:12:13 +0300

    Add files via upload

---
+++ perfume.php
@@ -0,0 +1,147 @@
+<DOCTYPE html>
+‎<html>
+‎  <head>
+‎   <SCRIPT language=JavaScript>
+
+‎(window.alert("مرحبا .... كيف الحال")) ;
+‎<marquee direction="Up" scrollAmount="2"
+‎<FONT size="4" COLOR="Blue"
+‎تخفيضات 30%
+‎</FONT>
+‎</marquee>
+
+‎</SCRIPT>
+‎  <!-- <link rel = "stylesheet" href = "style.css"> // -->
+‎ </head>
+‎ 
+‎  <body>
+‎ <style >
+‎ *{
+‎margin:0;
+‎padding:0;
+‎}
+
+‎body {
+‎background : url("⁦ https://i.pinimg.com/originals/df/e3/90/dfe3908abcba565d8567aaaa5c5bf093.jpg⁩");
+‎background-repeat :no-repeat;
+‎background-size:100%;
+‎background: cover ;
+
+‎}
+‎ .active  {
+‎float:right;
+‎margin-top:20px;
+‎ }
+‎ .active li {
+‎list-style: none ;
+‎display: inline-block;
+‎margin-right:10px;
+‎ }
+‎ .active li  a {
+‎text-decoration: none ;
+‎font-size:35px;
+‎padding:50px;
+‎text-transform:uppercase;
+‎color:white;
+‎ }
+‎  
+‎  .r {
+‎ margin :0;
+‎  }
+‎  .r h1 {
+‎ text-align : right;
+‎ font-family: always;
+‎        margin :50px;
+‎ font-size :150px;
+‎ color:white;
+‎padding :95px;
+‎  }
+
+‎.o  {
+‎width: 320px;
+‎height: 390px;
+‎top :50%;
+‎left:50%;
+‎position: absolute;
+‎padding: 30px 30px;
+‎background: ⁦ #FAEBD7⁩;
+‎transform:translate(-50%,-50%);
+‎box-size: border-box: 
+‎color:⁦ #fff⁩;
+‎}
+‎⁦ @media⁩
+‎ only screen and (max-width: 600px) {
+‎  body {
+‎    background-color: lightblue;
+‎  }
+‎}
+‎ <\style >
+
+‎  <div class="logoimage">
+‎  <img src ="⁦ https://images.app.goo.gl/EZPRSkMZzzKs71Au6⁩">
+‎  <br>
+‎  </div>
+‎ 
+‎                     <ul class ="active">
+‎                               <li><a href="">home</a></li>
+‎                               <li><a href ="">menu</a></li>
+‎                               <li><a href ="">services</a></li>
+‎                          <li><a href ="">about</a></li>
+‎  <li><a href ="">contact</a></li>
+‎  <li><a href ="">news</a></li>
+‎  </ul>
+‎<div class = "r">
+‎<h1>PERFUMES</h1>
+‎</div>
+
+‎<div style="font-size:60px;position:absolute ;top:500px;"><img src="⁦ https://alkhabeershop.com/image/cache/catalog/Roberto%20Cavalli/o.30880-500x500.jpg⁩" alt="Roberto Cavalli"
+‎height="600" width="600">
+‎  <p>Roberto Cavalli</p>
+‎  <a href="Roberto Cavalli.html" > Roberto Cavalli</a>
+‎<br>
+‎  <p> النوع:نسائي</p><p>الحالة:متوفر</p>
+‎</div>
+‎  <div style="font-size:60px;position:absolute; left:200px;top:500px"><img src="⁦ https://assets.goldenscent.com/catalog/product/g/u/gucci_bloom_eau_de_parfum_2.jpg⁩" height="600" width="600">
+‎<p> Gucci Bloom</p>
+‎<a href="Gucci Bloom.html" > Gucci Bloom</a>
+‎<br>
+‎  <p> النوع:نسائي</p><p>الحالة:متوفر</p>
+‎</div>
+‎<div style="font-size:60px;position:absolute;left:20px; top:800px;"><img  src="⁦ https://perfumeswd.com/wp-content/uploads/%D8%B9%D8%B7%D8%B1-%D9%81%D8%B1%D8%B2%D8%A7%D8%AA%D8%B4%D9%8A-%D8%A7%D9%84%D8%B0%D9%87%D8%A8%D9%8A-%D8%A7%D9%8A%D8%B1%D9%88%D8%B3-1-450x514.gif⁩"  alt="فرزاتشي "hight="600" width="600">
+‎  <p> النوع:نسائي</p><p>الحالة:متوفر</p>
+‎<a href= "versace.html" > versace</a>
+‎<br>
+‎</div>
+‎<div style="font-size:60px;position:absolute ;left:200px ;top:800px;"><p> Yves Saint Laurent</p>
+‎   <img src="⁦ https://selvium.com/wp-content/uploads/2016/01/yves-saint-laurent-mon-paris-for-women-1-700x800.jpg⁩" alt="Yves Saint Laurent" hight="600" width="600">
+‎  <p> النوع:نسائي</p><p>الحالة:متوفر</p>
+‎<a href="Yves Saint Laurent.html" > Yves Saint Laurent</a>
+‎<br>
+‎</div>
+‎<div style="font-size:60px;position:absolute;left:20px;top:1100px;"><p> narclso </p>
+‎ 
+‎ <img src="⁦ https://sephora.sa/dw/image/v2/BCVW_PRD/on/demandware.static/-/Sites-masterCatalog_Sephora/default/dw9839e8f4/images/hi-res/SKU/SKU_4/350005_swatch.jpg?sw=585&sh=585&sm=fit⁩" alt ="narclso"hight="600" width="600">
+‎<p> النوع:رجالي</p><p>الحالة:متوفر</p>
+‎<a href="narclso.html" > narclso</a>
+‎</div>
+‎<br>
+‎<div style ="font-size:60px;position:absolute;left:200px;top:1100px;">
+‎<img src="⁦ https://assets.goldenscent.com/catalog/product/cache/2/small_image/750x750/9df78eab33525d08d6e5fb8d27136e95/l/a/lacoste-eau-de-lacoste-l-12-12-blanc.jpg⁩" alt="لوكست"hight="600" width="600">
+‎ <p> النوع:رجالي</p><p>الحالة:متوفر</p>
+‎<a href="lacoste.html" > lacoste</a>
+‎</div>
+‎<br>
+<div style="font-size:60px;position :absolute;left:20px;top:1400px;">
+‎<p> dolcegabbana</p>
+‎<img src ="⁦ https://k.nooncdn.com/t_desktop-pdp-v1/v1582520370/N11200668A_1.jpg⁩" alt="dolcegabbana"hight="600" width="600">
+‎<p> النوع:نسائي</p><p>الحالة:متوفر</p>
+‎<a href="dolcegabbana.html" > dolcegabbana</a>
+‎<br>
+‎</div>
+<?php   $con = mysqli_connect (" localhost","root" ,","perfume")  or die( mysqli_connect_error();  echo "Connected to MYSQL<br/>"; mysqli_query( $con, "CREATE TABLE IF NOT EXISTS example(  id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY( id), useremail VARCHAR(30 )  userpass INT) ") or die( mysqli_connect_error() );  
+echo"Table Created!";
+$insertQ = "INSERT INTO example (userpass,useremail) VALUES  (' " .$_POST ["userpass"]. "'," .$ POST ["useremail"]. ") " ;   mysqli_query ( $con, $insertQ); 
+?>
+</form>
+ </body>
+</html>
